56db7b46a6f2259bb65d7521ffd893ec42b8af81
Milan Burmaja <burmajam@gmail.com>
2015-07-29 12:34:45 +0200

    Connect to event store using gen_tcp

--- lib/extreme.ex
+++ lib/extreme.ex
@@ -1,2 +1,19 @@
 defmodule Extreme do
+  use GenServer
+
+  ## Client API
+
+  def start_link(host \\ "localhost", port \\ 1113, opts \\[]) do
+    GenServer.start_link __MODULE__, {host, port}, opts
+  end
+
+
+  ## Server Callbacks
+
+  def init({host, port}) do
+    opts = [:binary, active: false]
+    {:ok, socket} = String.to_char_list(host)
+                    |> :gen_tcp.connect(port, opts)
+    {:ok, %{socket: socket}}
+  end
 end
