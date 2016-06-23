65fb59eab381c5b63538cc8d6b2455ea67553280
Milan Burmaja <burmajam@gmail.com>
2016-06-23 10:33:45 +0200

    upgraded version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Extreme.Mixfile do
 
   def project do
     [app: :extreme,
-     version: "0.5.1",
+     version: "0.5.2",
      elixir: ">= 1.0.0 and <= 1.3.0",
      source_url: "https://github.com/exponentially/extreme",
      description: """
